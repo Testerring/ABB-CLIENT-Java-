138114bf34afb628ef4d1ba474cf301b1976dcc7
NoxarX <jasongessler22@gmail.com>
2026-04-06 21:02:21 +0200

    Update FastPlaceModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/FastPlaceModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/FastPlaceModule.java
@@ -1,18 +1,24 @@
 package me.alpha432.oyvey.features.modules.player;
 
 import me.alpha432.oyvey.features.modules.Module;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 
 public class FastPlaceModule extends Module {
+
     public FastPlaceModule() {
-        super("FastPlace", "Makes you throw exp faster", Category.PLAYER);
+        super("FastPlace", "Makes you place blocks and throw exp faster", Category.PLAYER);
     }
 
     @Override
     public void onTick() {
         if (nullCheck()) return;
 
-        if (mc.player.isHolding(Items.EXPERIENCE_BOTTLE)) {
+        Item heldItem = mc.player.getMainHandItem().getItem();
+
+        // Check if the player is holding a block (for placing) or experience bottle (for throwing)
+        if (heldItem instanceof BlockItem || heldItem == Items.EXPERIENCE_BOTTLE) {
             mc.rightClickDelay = 0;
         }
     }
